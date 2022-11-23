2aa203aca9b95347edefd371958b7ff268a92a32
Luana Araruna Cabral <111820291+CodesdaLu@users.noreply.github.com>
2022-11-23 20:14:59 -0300

    add abilities

--- js/main.js
+++ js/main.js
@@ -20,6 +20,15 @@ function convertPokemonToLi(pokemon) {
                 <img src="${pokemon.photo}"
                      alt="${pokemon.name}">
             </div>
+            <p>Abilities</p>
+            
+            <div class="detail">
+        
+                <ol class="abilities">
+                ${pokemon.abilities.map((ability) => `<li class="ability">${ability}</li>`).join('')}
+                </ol>
+        
+            </div>                        
         </li>
     `
 }
@@ -48,6 +57,8 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
+
 
 
 
